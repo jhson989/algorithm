cd6ff3cb4a84759b5d7964308c0795d7fed4b8f6
Janghyun Son <jhson989@gmail.com>
2020-12-13 02:01:44 +0900

    stack updated

--- STL/stack/stack_seq.cpp
+++ STL/stack/stack_seq.cpp
@@ -1,6 +1,40 @@
 #include <cstdio>
 
+int m[100001];
+
 int main(void) {
+
+    bool good = true;
+    int N, max = 0, min = 100001;
+    scanf("%d", &N);
+    for (int i=0; i<N; i++) {
+        scanf("%d", &m[i]);
+        if (max < m[i]) {
+            max = m[i];
+            min = m[i];
+        }
+        min = min < m[i] ? min : m[i];
+        if (min < m[i] && m[i] < max)
+            good = false;
+    }
+    
+
+    if (good == false) {
+        printf("NO\n");
+        return 0;
+    }
+
+    max = 0;
+    for (int i=0; i<N; i++) {
+        int now = m[i];
+
+        if (max < m[i]) {
+            for (int j=max+1; j<=now; j++)
+                printf("+\n");
+            max = m[i];
+        }
+        printf("-\n");
+    }
 
     return 0;
 }
