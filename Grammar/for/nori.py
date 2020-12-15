c3b7872a34773acb108932a2c07eebc263328894
Janghyun Son <jhson989@gmail.com>
2020-12-15 19:09:14 +0900

    2020-12-15

--- Grammar/for/nori.py
+++ Grammar/for/nori.py
@@ -1,2 +1,16 @@
 import sys
-sys.stdin.readline
+N, X = map(int, sys.stdin.readline().split())
+array = sys.stdin.readline().split()
+array = [ int(e) for e in array ]
+
+for e in array:
+    if (e<X):
+        print(e, end=" ")
+
+
+'''
+10 5
+1 10 4 9 2 3 8 5 7 6
+예제 출력 1 
+1 4 2 3
+'''
